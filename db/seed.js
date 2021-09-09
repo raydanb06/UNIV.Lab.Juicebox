d696f023a3aad127de12f4589fec97e9e38d431b
Raydan Bala <raydanb@gmail.com>
2021-09-08 21:02:09 -0700

    finished createTables function

--- db/seed.js
+++ db/seed.js
@@ -26,7 +26,11 @@ dropTables = async () => {
 createTables = async () => {
   try {
     await client.query(`
-
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar(255) NOT NULL
+      );
   `);
   } catch (error) {
     throw error;
