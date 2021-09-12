addb023e5bc09477a31b5603112c86ec75e246b0
Raydan Bala <raydanb@gmail.com>
2021-09-12 07:55:47 -0700

    getPostsById

--- db/index.js
+++ db/index.js
@@ -189,6 +189,38 @@ const addTagToPost = async (postId, tagList) => {
   }
 }
 
+const getPostsById = async (postId) => {
+  try {
+    const { rows: [ post ] } = await client.query(`
+    SELECT *
+    FROM posts
+    WHERE id = $1;
+    `, [postId]);
+
+    const { rows: tags } = await client.query(`
+      SELECT tags.*
+      FROM tags
+      JOIN post_tags on tags.id=post_tags."tagId"
+      WHERE post_tags."postId" = $1;
+    `, [postId]);
+
+    const { rows: [ author ] } = await client.query(`
+      SELECT id, username, name, location
+      FROM users
+      WHERE id = $1;
+    `, [post.authorId]);
+
+    post.tags = tags;
+    post.author = author;
+
+    delete post.authorId;
+
+    return post;
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   client,
   getAllUsers,
