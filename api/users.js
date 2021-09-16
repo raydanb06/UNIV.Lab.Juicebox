ecb1e39cd32672ea5899d26905af3b6bca1f3f00
Raydan Bala <raydanb@gmail.com>
2021-09-15 21:22:04 -0700

    create user in /user/register route

--- api/users.js
+++ api/users.js
@@ -4,7 +4,8 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = process.env;
 
 const { getAllUsers,
-        getUserByUsername
+        getUserByUsername,
+        createUser
 } = require('../db');
 
 usersRouter.use((req, res, next) => {
@@ -49,6 +50,42 @@ try {
   } catch(error) {
     console.log(error);
     next(error);
+  }
+});
+
+usersRouter.post('/register', async (req, res, next) => {
+  const { username, password, name, location } = req.body;
+
+  try {
+    const _user = await getUserByUsername(username);
+
+    if (_user) {
+      next({
+        name: 'UsernameExistsError',
+        message: 'A user by that username already exists'
+      });
+    }
+
+    const user = await createUser({
+      username,
+      password,
+      name,
+      location
+    });
+
+    const token = jwt.sign({
+      id: user.id,
+      username
+    }, process.env.JWT_SECRET, {
+      expiresIn: '1w'
+    });
+
+    res.send({
+      message: "Thank you for signing up",
+      token
+    })
+  } catch ({ name, message }) {
+    next({ name, message })
   }
 });
 
