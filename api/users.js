db51495a0538d3f0a2e23c66de39583f1d2c332b
Raydan Bala <raydanb@gmail.com>
2021-09-15 20:06:07 -0700

    added login route for users, added function getUserByUsername in db/index.js

--- api/users.js
+++ api/users.js
@@ -18,4 +18,9 @@ usersRouter.get('/', async (req, res) => {
   });
 });
 
+usersRouter.post('/login', async (req, res, next) => {
+  console.log(req.body);
+  res.end();
+});
+
 module.exports = usersRouter;
