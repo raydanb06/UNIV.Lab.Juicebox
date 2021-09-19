8767af366142c6aa5fc862031f1694765ed1f79a
Raydan Bala <raydanb@gmail.com>
2021-09-19 10:54:48 -0700

    DELETE post complete

--- api/posts.js
+++ api/posts.js
@@ -85,4 +85,28 @@ postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
     next({ name, message });
   }
 });
+
+postsRouter.delete('/:postId', requireUser, async (req, res, next) => {
+  try {
+    const post = await getPostById(req.params.postId);
+
+    if ( post && post.author.id === req.user.id ) {
+      const updatedPost = await updatePost(post.id, { active: false});
+
+      res.send({ post: updatedPost});
+    } else {
+      next( post ? {
+        name: 'UnauthorizedUserError',
+        message: 'You cannot delete a post which is not yours'
+      } : {
+        name: 'PostNotFoundError',
+        message: 'That post does not exist'
+      }
+      )
+    }
+  } catch ({ name, message }) {
+    next({ name, message })
+  }
+});
+
 module.exports = postsRouter;
